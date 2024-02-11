cdf8350a64a854105adebc58601e9cf9ce339461
Gus <gustavo.silvapes2020@gmail.com>
2024-02-11 16:16:03 -0300

    vercel

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const routes = require('./src/routes');
-const config = require('./src/config/index');
-
 const app = express();
 mongoose.connect(process.env.MONGO_URL)
 
 app.use(cors());
 
 app.use('/api', routes);
+
+app.get('/', (req, res) => {
+    res.status(200).json('Welcome, your app is working well');
+})
 
 app.listen(process.env.PORT);
