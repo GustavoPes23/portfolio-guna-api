aa233c2dfe2ccb37478fd019194d8cc3c70c5800
Gus <gustavo.silvapes2020@gmail.com>
2024-02-11 16:45:19 -0300

    Teste

---
+++ api/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config({ path: '../.env' });
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('../src/routes');
+const app = express();
+mongoose.connect(process.env.MONGO_URL);
+
+app.use(cors());
+
+app.use('/', routes);
+
+app.listen(process.env.PORT);
