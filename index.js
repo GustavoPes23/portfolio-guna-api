98df6c2daf43436ce018cf4eec3a43423aedc70b
Gus <gustavo.silvapes2020@gmail.com>
2024-02-12 13:44:55 -0300

    teste

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config({ path: './.env' });
+
+const express = require('express');
+// const cors = require('cors');
+// const routes = require('./src/routes');
+
+const app = express();
+// app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+// app.use('/api', routes);
+
+app.listen(process.env.PORT);
+
+module.exports = app;
