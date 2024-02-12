d0457cf2be379d65dc04fbbe45cc0beea546ee7a
Gus <gustavo.silvapes2020@gmail.com>
2024-02-12 13:29:46 -0300

    Fix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require('dotenv').config({ path: './.env' });
+// require('dotenv').config({ path: './.env' });
 
 const express = require('express');
 const cors = require('cors');
 const routes = require('./src/routes');
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 
 app.use('/api', routes);
 
