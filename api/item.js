b086cab71c1c4edd47a1c4b5a04db5770b3a9e71
Gus <gustavo.silvapes2020@gmail.com>
2024-02-12 14:06:56 -0300

    teste

--- api/item.js
+++ api/item.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const multer = require("multer");
-const itemMulterConfig = require("./config/itemMulterConfig")
+const itemMulterConfig = require("../src/config/itemMulterConfig")
 const routes = express.Router();
 
-const ItemService = require('./services/item');
+const ItemService = require('../src/services/item');
 
 routes.post('/', multer(itemMulterConfig).single("file"), ItemService.doPost)
     .get('/:id', ItemService.doGetById)
